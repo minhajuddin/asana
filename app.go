46a4471872042ef77af04341bf09e6b332089719
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2014-04-13 19:04:17 +0530

    Added ability to set sub commands

--- app.go
+++ app.go
@@ -9,13 +9,13 @@ import (
 func main() {
 	loadConfig()
 	//-r should refresh the metadata
+	parseOptions()
+}
+
+func list() {
 	loadProjects()
-	//TODO handle invalid args
-	if len(os.Args) < 2 {
-		fmt.Println("Invalid number of args")
-		os.Exit(1)
-	}
-	match := strings.Join(os.Args[1:], " ")
+	match := strings.Join(os.Args[2:], " ")
+	fmt.Println(match)
 	project := projects.find(match)
 	if project == nil {
 		fmt.Println("Project not found")
@@ -25,4 +25,26 @@ func main() {
 	for i, t := range project.tasks() {
 		fmt.Printf("%v. %v\n", i+1, t.Name)
 	}
+}
+
+var commands = map[string]func(){
+	"list": list,
+}
+
+func parseOptions() {
+	if len(os.Args) < 2 {
+		invalidArgsHandler()
+	}
+
+	command, ok := commands[os.Args[1]]
+	if !ok {
+		invalidArgsHandler()
+	}
+	command()
+}
+
+func invalidArgsHandler() {
+	fmt.Println("Invalid args")
+	fmt.Println("Usage: asana list <project-name>")
+	os.Exit(1)
 }
